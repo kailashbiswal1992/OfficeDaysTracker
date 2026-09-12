01153842beb50f2f2ac325182080386e82e7bb3c
kailashbiswal1992 <64208998+kailashbiswal1992@users.noreply.github.com>
2026-09-12 10:23:21 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase web configuration.
+// This identifies the Firebase project; it is not a private server secret.
+const firebaseConfig = {
+  apiKey: "AIzaSyDykGxIq2GUDxFkv28NLmdRmiQzlEjvFuw",
+  authDomain: "rento-56bca.firebaseapp.com",
+  projectId: "rento-56bca",
+  storageBucket: "rento-56bca.appspot.com",
+  messagingSenderId: "641174222477",
+  appId: "1:641174222477:web:0c6c5999787c2ca05d14c4",
+  measurementId: "G-T3CSB1Z271"
+};
